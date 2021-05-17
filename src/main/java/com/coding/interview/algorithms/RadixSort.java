226ee812dc6f784c98646f9adda0170e23cd1278
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-05-17 16:15:49 +0300

    add radix sort for strings(letters)

--- src/main/java/com/coding/interview/algorithms/RadixSort.java
+++ src/main/java/com/coding/interview/algorithms/RadixSort.java
@@ -9,7 +9,22 @@ public class RadixSort {
         radixSort(radixArray, 10, 4);
 
         for (int j : radixArray) {
-            System.out.print(j + " ");
+            System.out.println(j);
+        }
+
+        String[] radStringArray = { "bcdef", "dbaqc", "abcde", "omadd", "bbbbb" };
+
+        radixSort(radStringArray, 26, 5);
+
+        for (String j : radStringArray) {
+            System.out.println(j);
+        }
+    }
+
+    public static void radixSort(String[] input, int radix, int width) {
+
+        for (int i = width - 1; i >= 0; i--) {
+            radixSingleSort(input, i, radix);
         }
     }
 
@@ -18,6 +33,36 @@ public class RadixSort {
          for (int i = 0; i < width; i++) {
              radixSingleSort(input, i, radix);
          }
+    }
+
+    private static void radixSingleSort(String[] input, int position, int radix) {
+
+        int numItems = input.length;
+
+        int[] countArray = new int[radix];
+
+        for (String value : input) {
+            countArray[getIndex(position, value)]++;
+        }
+
+        // Adjust the count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        String [] temp = new String[numItems];
+
+        for (int tempIndex = numItems - 1; tempIndex  >= 0; tempIndex--) {
+            temp[--countArray[getIndex(position, input[tempIndex])]] = input[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex ++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    private static int getIndex(int position, String value) {
+        return value.charAt(position) - 'a';
     }
 
     private static void radixSingleSort(int[] input, int position, int radix) {
