211ae8a787fb4d435c9b6ebe5e859440dd275078
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-05-17 15:37:36 +0300

    add recursive insertion sort

--- src/main/java/com/coding/interview/algorithms/InsertionSort.java
+++ src/main/java/com/coding/interview/algorithms/InsertionSort.java
@@ -6,21 +6,57 @@ public class InsertionSort {
 
         int[] intArray = {20, 35, -15, 7, 55, 1, -22};
 
-        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+        System.out.println("Iteration insertion sort");
 
-            int newElement = intArray[firstUnsortedIndex];
-
-            int i;
-
-            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
-                intArray[i] = intArray[i - 1];
-            }
-
-            intArray[i] = newElement;
-        }
+        insertionSort(intArray);
 
         for (int j : intArray) {
             System.out.print(j + " ");
+        }
+
+        System.out.println("\n" + "Recursion insertion sort");
+
+        int[] intArray2 = {20, 35, -15, 7, 55, 1, -22};
+
+        recursiveInsertionSort(intArray2, intArray2.length);
+
+        for (int j : intArray2) {
+            System.out.print(j + " ");
+        }
+    }
+
+    public static void recursiveInsertionSort(int[] input, int numberOfItems) {
+
+        if (numberOfItems < 2) {
+            return;
+        }
+
+        recursiveInsertionSort(input, numberOfItems -1);
+
+        int newElement = input[numberOfItems - 1];
+
+        int i;
+
+        for (i = numberOfItems - 1; i > 0 && input[i - 1] > newElement; i--) {
+            input[i] = input[i - 1];
+        }
+
+        input[i] = newElement;
+    }
+
+    public static void insertionSort(int[] input) {
+
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < input.length; firstUnsortedIndex++) {
+
+            int newElement = input[firstUnsortedIndex];
+
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && input[i - 1] > newElement; i--) {
+                input[i] = input[i - 1];
+            }
+
+            input[i] = newElement;
         }
     }
 }
