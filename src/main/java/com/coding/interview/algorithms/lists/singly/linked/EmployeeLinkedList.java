c9e2e0da7f6abbe2a78da0ca4359bfd42b61f4de
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-05-19 10:31:05 +0300

    remove redundant import

--- src/main/java/com/coding/interview/algorithms/lists/singly/linked/EmployeeLinkedList.java
+++ src/main/java/com/coding/interview/algorithms/lists/singly/linked/EmployeeLinkedList.java
@@ -1,7 +1,6 @@
 package com.coding.interview.algorithms.lists.singly.linked;
 
 import com.coding.interview.algorithms.lists.Employee;
-import com.coding.interview.algorithms.lists.singly.linked.EmployeeNode;
 
 public class EmployeeLinkedList {
 
