5e7f638d344846b29a1fe6ad774e9470ad573f1b
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-05-26 07:34:07 +0300

    add heap implementation

---
+++ src/main/java/com/coding/interview/algorithms/heaps/HeapMain.java
@@ -0,0 +1,36 @@
+package com.coding.interview.algorithms.heaps;
+
+public class HeapMain {
+
+    public static void main(String[] args) {
+
+        Heap heap = new Heap(10);
+
+        heap.insert(80);
+        heap.insert(75);
+        heap.insert(68);
+        heap.insert(60);
+        heap.insert(55);
+        heap.insert(40);
+        heap.insert(52);
+        heap.insert(67);
+
+        System.out.println(heap.peek());
+
+        heap.printHeap();
+
+        heap.delete(1);
+
+        heap.printHeap();
+
+        heap.delete(5);
+
+        heap.printHeap();
+
+        heap.delete(0);
+
+        heap.printHeap();
+
+        System.out.println(heap.peek());
+    }
+}
