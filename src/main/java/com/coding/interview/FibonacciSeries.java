0eb6b7848921fd9a711a9952014685ca7a17273a
Oleg Bezkorovaynyi <oleg.bezkorovajniy@gmail.com>
2021-05-30 23:50:28 +0300

    add dynamic fibonacci series

--- src/main/java/com/coding/interview/FibonacciSeries.java
+++ src/main/java/com/coding/interview/FibonacciSeries.java
@@ -8,7 +8,7 @@ public class FibonacciSeries {
 
     public static void main(String[] args) {
         //input to print Fibonacci series upto how many numbers
-        System.out.println("Enter number upto which Fibonacci series to print: ");
+        System.out.println("Enter number up to which Fibonacci series to print: ");
         int number = new Scanner(System.in).nextInt();
 
         System.out.println("Fibonacci series upto " + number + " numbers : ");
@@ -34,6 +34,13 @@ public class FibonacciSeries {
         return fibonacci(number - 1) + fibonacci(number - 2);
     }
 
+    /**
+     * Map implementation with O(n) time complexity and O(n) stack size in memory
+     *
+     * @param number number
+     * @param memo   memorize container
+     * @return fib number
+     */
     public static long fib(int number, Map<Integer, Long> memo) {
 
         if (memo.containsKey(number)) {
@@ -49,8 +56,33 @@ public class FibonacciSeries {
         return memo.get(number);
     }
 
-    /*
+
+    /**
+     * Using array instead of Map to memorize calculations
+     */
+    private Integer[] cache = new Integer[31];
+
+    public int fib(int number) {
+        if (number <= 1) {
+            return number;
+        }
+        cache[0] = 0;
+        cache[1] = 1;
+        return memorize(number);
+    }
+
+    public int memorize(int number) {
+        if (cache[number] != null) {
+            return cache[number];
+        }
+        cache[number] = memorize(number - 1) + memorize(number - 2);
+        return memorize(number);
+    }
+
+    /**
      * Java program to calculate Fibonacci number using loop or Iteration.
+     * Time complexity is O(n) and space complexity is O(1) - variables fibo1, fibo2 & fibonacci will be reused
+     *
      * @return Fibonacci number
      */
     public static int fibonacci2(int number) {
@@ -60,13 +92,25 @@ public class FibonacciSeries {
         int fibo1 = 1, fibo2 = 1, fibonacci = 1;
         for (int i = 3; i <= number; i++) {
 
-            //Fibonacci number is sum of previous two Fibonacci number
+            //Fibonacci number is sum of previous two Fibonacci numbers
             fibonacci = fibo1 + fibo2;
             fibo1 = fibo2;
             fibo2 = fibonacci;
 
         }
-        return fibonacci; //Fibonacci number
+        return fibonacci;
 
+    }
+
+    /**
+     * Use the golden ratio formula to calculate the Nth Fibonacci number
+     * Time and space complexity is O(1) both
+     *
+     * @param number num
+     * @return fib num
+     */
+    public int goldenFib(int number) {
+        double goldenRatio = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(goldenRatio, number) / Math.sqrt(5));
     }
 }
